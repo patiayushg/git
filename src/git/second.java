46395c4fe334dccd891088fe46e587e6b856e8b3
Ayush Patidar <Ayush Patidar@Dell-5000>
2019-07-26 10:04:28 +0530

    My Second Commit

---
+++ src/git/second.java
@@ -0,0 +1,13 @@
+package git;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 2;
+		if(i%2==1) System.out.println("Odd");
+		else System.out.println("Even");
+
+	}
+
+}
